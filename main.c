991768f3246953f111406d52a1e629b8278acd1c
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-06 12:58:21 +0000

    copied program

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
+// Function to copy one string to another. Pointer Ver. 2
+
 #include <stdio.h>
 
+// Declare Functions
+void copyString (char *, char *);
+
 int main(void) {
-  printf("Hello World\n");
+
+  char string1[] = "A string to be copied.";
+  char string2[50];
+
+  copyString (string2, string1);
+  printf ("%s\n", string2);
+
+  copyString (string2, "So is this.");
+  printf ("%s\n", string2);
+  
   return 0;
+}
+
+// Define Functions
+void copyString (char *to, char *from) {
+  while (*from) 
+    *to++ = *from++;
+
+  *to = '\0';
 }
